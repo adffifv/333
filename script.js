d18d0783d5d5d09ba595379b5b555b8d23c18f4f
adffifv <2812341486@qq.com>
2024-09-18 20:31:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {  
-    console.log('页面已加载完毕！');  
-
-    // 示例：点击图片时，在控制台输出信息  
-    const img = document.querySelector('img');  
-    img.addEventListener('click', function() {  
-        console.log('你点击了图片！');  
-    });  
-});
+let currentIndex = 0;  
+const images = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg", "img5.jpg"];  
+  
+function updateImage() {  
+    const imgElement = document.getElementById('current-image');  
+    imgElement.src = `images/${images[currentIndex]}`;  
+}  
+  
+function nextImage() {  
+    currentIndex = (currentIndex + 1) % images.length;  
+    updateImage();  
+}  
+  
+function prevImage() {  
+    currentIndex = (currentIndex - 1 + images.length) % images.length;  
+    updateImage();  
+}  
+  
+// 初始加载时显示第一张图片  
+updateImage();
